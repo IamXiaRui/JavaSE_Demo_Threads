569e5403e0b2b1516a15b3f38856a60799503c73
IamXiaRui <iamxiarui@foxmail.com>
2016-04-06 22:33:17 +0800

    网页下载客户端

--- xr_java_threads/src/xr_threads_download/UploadForNetDemo.java
+++ xr_java_threads/src/xr_threads_download/UploadForNetDemo.java
@@ -7,39 +7,55 @@ import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
 
+/**
+ * @ClassName: UploadForNetDemo
+ * @Description: 多线程断点下载文件
+ * @author iamxiarui@foxmail.com
+ * @date 2016年4月6日 下午10:27:47
+ * 
+ */
 public class UploadForNetDemo {
 
+	// 文件下载路径
 	private static String DOWN_PATH = "http://172.25.10.172:8080/Web_UploadDemo/testApp.exe";
 
+	// 线程的数量
 	private static int THREAD_COUNT = 3;
 
+	// 每个线程需要下载文件的大小
 	private static int BLOCK_SIZE = 0;
 
 	public static void main(String[] args) {
 
 		try {
 			URL url = new URL(DOWN_PATH);
-
 			HttpURLConnection openConnection = (HttpURLConnection) url.openConnection();
-
 			openConnection.setRequestMethod("GET");
 			openConnection.setConnectTimeout(10 * 1000);
 
 			int responseCode = openConnection.getResponseCode();
 
 			if (responseCode == 200) {
+				// 得到文件的长度
 				int fileLength = openConnection.getContentLength();
 				RandomAccessFile randomAccessFile = new RandomAccessFile(new File("E:\\AllTestFile\\testApp.exe"),
 						"rw");
+				// 给下载的文件预留空间
 				randomAccessFile.setLength(fileLength);
 
+				// 每个线程所需下载的大小为 文件大小 除以 线程数量
 				BLOCK_SIZE = fileLength / THREAD_COUNT;
 
+				// 循环开启线程
 				for (int THREAD_ID = 0; THREAD_ID < THREAD_COUNT; THREAD_ID++) {
+					// 线程下载起始位置
 					int START_INDEX = THREAD_ID * BLOCK_SIZE;
+					// 线程下载结束位置
 					int END_INDEX = (THREAD_ID + 1) * BLOCK_SIZE - 1;
 
+					// 如果是最后一个线程
 					if (THREAD_ID == THREAD_COUNT - 1) {
+						// 结束位置为文件大小-1
 						END_INDEX = fileLength - 1;
 					}
 
@@ -55,6 +71,13 @@ public class UploadForNetDemo {
 
 	}
 
+	/**
+	 * @ClassName: UploadThread
+	 * @Description:多线程下载
+	 * @author iamxiarui@foxmail.com
+	 * @date 2016年4月6日 下午10:30:54
+	 * 
+	 */
 	public static class UploadThread extends Thread {
 
 		private int threadId;
@@ -70,22 +93,24 @@ public class UploadForNetDemo {
 		@Override
 		public void run() {
 
-			URL url;
 			try {
-				url = new URL(DOWN_PATH);
+				URL url = new URL(DOWN_PATH);
 				HttpURLConnection openConnection = (HttpURLConnection) url.openConnection();
 				openConnection.setRequestMethod("GET");
 				openConnection.setConnectTimeout(10 * 1000);
 
 				System.out.println("线程： " + threadId + ",开始位置： " + startIndex + ",结束位置：" + endIndex);
 
+				// 文件头信息 必须的格式
 				openConnection.setRequestProperty("Range", "bytes:" + startIndex + "-" + endIndex);
 				int responseCode = openConnection.getResponseCode();
+				// 200 为全部资源请求信息 206 为部分资源请求信息
 				if (responseCode == 206) {
 					InputStream inputStream = openConnection.getInputStream();
 
 					RandomAccessFile randomAccessFile = new RandomAccessFile(new File("E:\\AllTestFile\\testApp.exe"),
 							"rw");
+					// 从文件的偏移量开始存储
 					randomAccessFile.seek(startIndex);
 
 					byte[] buf = new byte[1024];
